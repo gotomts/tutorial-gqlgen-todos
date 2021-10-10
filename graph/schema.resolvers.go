8ea3b0fc8fad249d58a9fa21d19a1f56fcc55163
Mitsuharu Goto <mh.goto.web@gmail.com>
2021-10-11 07:54:15 +0900

    feat: 新しいresolverの修正

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -16,7 +16,7 @@ func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) 
 	todo := &model.Todo{
 		Text: input.Text,
 		ID:   fmt.Sprintf("T%d", rand.Int()),
-		User: &model.User{ID: input.UserID, Name: "user" + input.UserID},
+		UserID: input.UserID,
 	}
 	r.todos = append(r.todos, todo)
 	return todo, nil
@@ -27,7 +27,7 @@ func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
 }
 
 func (r *todoResolver) User(ctx context.Context, obj *model.Todo) (*model.User, error) {
-	panic(fmt.Errorf("not implemented"))
+	return &model.User{ID: obj.UserID, Name: "user " + obj.UserID}, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
